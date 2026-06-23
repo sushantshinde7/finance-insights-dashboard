a0217fd2f4fe920ea8ca95604e3f7e1d91d97274
Sushant Shinde <sushantshinde2024@gmail.com>
2026-06-24 01:22:33 +0530

    refactor: migrate theme, transactions and auth logic to contexts and remove obsolete files

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/global.css";
 
+import { ThemeProvider } from "./context/ThemeContext";
+import { TransactionProvider } from "./context/TransactionContext";
+import { AuthProvider } from "./context/AuthContext";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider>
+      <TransactionProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </TransactionProvider>
+    </ThemeProvider>
+  </React.StrictMode>,
 );
